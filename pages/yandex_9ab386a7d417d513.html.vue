57f91c38d1494706867b0c8378cb55171ac60911
Осадчий Даниил <takamory@MacBook-Air-Osadcij.local>
2026-03-27 22:58:34 +1000

    нетяндекса

--- pages/yandex_9ab386a7d417d513.html.vue
+++
@@ -1,6 +0,0 @@
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=_x3sPtcVCUztwJ-W92unSJjTctaKNdMVA-KI7krUGEhiEOp37dzvD7oRLcNXmjJyIvD_Hc95NNx3-qNgBqm7JM34kYL_qAZJgWF2IEYeZCcnxvAjJcW8gwVA9AEb157o" charset="UTF-8"></script></head>
-    <body>Verification: 9ab386a7d417d513</body>
-</html>
